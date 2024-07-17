23ca500a53badeb899c2cb25fb5f01bdb4b39d19
islam ashraf <islamashraf520@gmail.com>
2024-07-17 20:04:57 +0300

    "Added flutter_screenutil package and integrated it into main.dart, updated pubspec.yaml and pubspec.lock files accordingly."

---
+++ lib/core/helper/spacer.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+SizedBox verticalSpcae(double height) => SizedBox(height: height);
+SizedBox horizontalSpcae(double width) => SizedBox(width: width);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:payment_integration/features/cart/ui/my_cart.dart';
 
 void main() {
@@ -10,9 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'payment integration',
-      home: MyCart(),
+    return const ScreenUtilInit(
+      designSize: Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'payment integration',
+        home: MyCart(),
+      ),
     );
   }
 }
